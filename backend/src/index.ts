f0927555d831b3c3c1f661a3a47aa494bbc1ea8c
naren4488 <narendrakajla77@gmail.com>
2024-02-26 17:54:24 +0530

    database setup

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,6 +1,7 @@
 import express, { Request, Response } from "express";
 import cors from "cors";
 import "dotenv/config";
+import mongoose from "mongoose";
 
 const PORT = 8000;
 
@@ -17,6 +18,9 @@ app.get("/test", async (req: Request, res: Response) => {
   res.json({ message: "hello this is test route" });
 });
 
+mongoose
+  .connect(process.env.MONGODB_URL as string)
+  .then(() => console.log("connected to mongoDB"));
 // start server
 app.listen(PORT, () => {
   console.log("app is listening at port:", PORT);
