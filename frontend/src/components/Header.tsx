ce74ffe1e780f53865bf529efd6bb101cb36031f
naren4488 <narendrakajla77@gmail.com>
2024-02-26 21:02:58 +0530

    mobile navbar

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,12 +1,19 @@
 import { Link } from "react-router-dom";
+import MobileNav from "./MobileNav";
 
 const Header = () => {
   return (
     <div className="border-b-2 border-b-red-500 py-6">
       <div className="container mx-auto flex items-center justify-between">
-        <Link className="text-3xl tracking-tight text-red-500" to={"/"}>
+        <Link
+          className="text-3xl font-semibold tracking-tight text-red-500"
+          to={"/"}
+        >
           FoodDeliveryApp
         </Link>
+        <div className="md:hidden">
+          <MobileNav />
+        </div>
       </div>
     </div>
   );
---
+++ frontend/src/components/MobileNav.tsx
@@ -0,0 +1,36 @@
+import { Menu } from "lucide-react";
+import {
+  Sheet,
+  SheetContent,
+  SheetDescription,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "./ui/sheet";
+import { Separator } from "./ui/separator";
+import { Button } from "./ui/button";
+
+const MobileNav = () => {
+  return (
+    <Sheet>
+      <SheetTrigger>
+        <div className="text-red-500">
+          <Menu />
+        </div>
+      </SheetTrigger>
+      <SheetContent>
+        <SheetHeader className=" mt-5 space-y-3">
+          <SheetTitle>
+            <span>Welcome to FoodDeliveryApp!</span>
+          </SheetTitle>
+          <Separator />
+          <SheetDescription>
+            <Button className=" w-full bg-red-500 font-bold">Log In</Button>
+          </SheetDescription>
+        </SheetHeader>
+      </SheetContent>
+    </Sheet>
+  );
+};
+
+export default MobileNav;
