1201b0d9575a8a9fd2e678815c75b3b772300a68
naren4488 <narendrakajla77@gmail.com>
2024-02-27 20:16:56 +0530

    validate token

--- backend/src/routes/myUser.route.ts
+++ backend/src/routes/myUser.route.ts
@@ -1,8 +1,9 @@
 import express from "express";
 import myUserController from "../controllers/myUser.controller";
+import { jwtCheck } from "../middleware/auth.middleware";
 
 const router = express();
 
-router.post("/", myUserController.createCurrentUser);
+router.post("/", jwtCheck, myUserController.createCurrentUser);
 
 export default router;
