6e47970d124e74cc6eb18efdcf9d3d180ae1abf3
naren4488 <narendrakajla77@gmail.com>
2024-02-27 14:37:44 +0530

    auth0 setup

--- frontend/src/components/Nav.tsx
+++ frontend/src/components/Nav.tsx
@@ -1,9 +1,14 @@
+import { useAuth0 } from "@auth0/auth0-react";
 import { Button } from "./ui/button";
 
 export const Nav = () => {
+  const { loginWithRedirect } = useAuth0();
   return (
     <div>
-      <Button className=" bg-red-500 font-bold text-white hover:bg-red-600 hover:text-white">
+      <Button
+        onClick={async () => await loginWithRedirect()}
+        className=" bg-red-500 font-bold text-white hover:bg-red-600 hover:text-white"
+      >
         Log In
       </Button>
     </div>
