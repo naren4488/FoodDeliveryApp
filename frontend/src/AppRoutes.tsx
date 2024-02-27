6c6a1f54ce15be3d555fef28e27f34854edeec3f
naren4488 <narendrakajla77@gmail.com>
2024-02-27 19:54:25 +0530

    get access token

--- frontend/src/AppRoutes.tsx
+++ frontend/src/AppRoutes.tsx
@@ -1,6 +1,7 @@
 import { Navigate, Route, Routes } from "react-router-dom";
 import Layout from "./layouts/layout";
 import HomePage from "./pages/HomePage";
+import AuthCallbackPage from "./pages/AuthCallbackPage";
 
 export const AppRoutes = () => {
   return (
@@ -13,6 +14,7 @@ export const AppRoutes = () => {
           </Layout>
         }
       />
+      <Route path="/auth-callback" element={<AuthCallbackPage />} />
       <Route path="/user-profile" element={<span>User Profile page</span>} />
       <Route path="*" element={<Navigate to={"/"} />} />
     </Routes>
