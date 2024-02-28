4bcb7a3b7fa5cc889c34bb03d5e4d532d959b13f
naren4488 <narendrakajla77@gmail.com>
2024-02-28 17:57:09 +0530

    get user API & pre-populate user form

--- backend/src/routes/myUser.route.ts
+++ backend/src/routes/myUser.route.ts
@@ -1,15 +1,16 @@
 import express from "express";
 import myUserController from "../controllers/myUser.controller";
-import { jwtCheck, jwtPaser } from "../middleware/auth.middleware";
+import { jwtCheck, jwtParse } from "../middleware/auth.middleware";
 import { validateMyUserRequest } from "../middleware/validation";
 
 const router = express();
 
+router.get("/", jwtCheck, jwtParse, myUserController.getCurrentUser);
 router.post("/", jwtCheck, myUserController.createCurrentUser);
 router.put(
   "/",
   jwtCheck,
-  jwtPaser,
+  jwtParse,
   validateMyUserRequest,
   myUserController.updateCurrentUser
 );
