08158836e137cd54d8f641efbe644daee36a6bae
naren4488 <narendrakajla77@gmail.com>
2024-03-01 13:42:06 +0530

    pre-populate restaurant form

--- frontend/src/auth/Auth0ProvideWithNav.tsx
+++ frontend/src/auth/Auth0ProvideWithNav.tsx
@@ -30,8 +30,8 @@ const Auth0ProviderWithNavigate = ({ children }: Props) => {
         redirect_uri: redirectUri,
         audience: audience,
       }}
-      // useRefreshTokens
-      // cacheLocation="localstorage"
+      useRefreshTokens
+      cacheLocation="localstorage"
       onRedirectCallback={onRedirectCallback}
     >
       {children}
