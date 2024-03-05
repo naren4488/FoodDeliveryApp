d34ff31206a7accaae21d3e1ddad4c8d956e18bd
naren4488 <narendrakajla77@gmail.com>
2024-03-05 17:53:14 +0530

    restaurant orders API

--- backend/src/controllers/myRestaurant.controller.ts
+++ backend/src/controllers/myRestaurant.controller.ts
@@ -2,6 +2,7 @@ import { Request, Response } from "express";
 import Restaurant from "../models/restaurant.model";
 import cloudinary from "cloudinary";
 import mongoose from "mongoose";
+import Order from "../models/order.model";
 
 const getMyRestaurant = async (req: Request, res: Response) => {
   try {
@@ -68,6 +69,24 @@ const updateMyRestaurant = async (req: Request, res: Response) => {
   }
 };
 
+const getMyRestaurantOrders = async (req: Request, res: Response) => {
+  try {
+    const restaurant = await Restaurant.findOne({ user: req.userId });
+    if (!restaurant) {
+      return res.status(404).json({ message: "restaurant not found" });
+    }
+
+    const orders = await Order.find({ restaurant: restaurant._id })
+      .populate("restaurant")
+      .populate("user");
+
+    res.json(orders);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: "something went wrong" });
+  }
+};
+
 const uploadImage = async (file: Express.Multer.File) => {
   const image = file;
   const base64Image = Buffer.from(image.buffer).toString("base64");
@@ -78,4 +97,9 @@ const uploadImage = async (file: Express.Multer.File) => {
   return uploadResponse.url;
 };
 
-export default { createMyRestaurant, getMyRestaurant, updateMyRestaurant };
+export default {
+  createMyRestaurant,
+  getMyRestaurant,
+  updateMyRestaurant,
+  getMyRestaurantOrders,
+};
